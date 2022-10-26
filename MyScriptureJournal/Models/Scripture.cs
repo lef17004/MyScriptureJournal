8c6b5e540cb282c8e163f82476dd413809f16a0d
Michael LeFevre <64995133+lef17004@users.noreply.github.com>
2022-10-26 16:34:28 -0600

    Commit 1

---
+++ MyScriptureJournal/Models/Scripture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyScriptureJournal.Models
+{
+    public class Scripture
+    {
+        public int ScriptureId { get; set; }
+        public string Book { get; set; }
+        public int Chapter { get; set; }
+        public string Verses { get; set; }
+        public string Notes { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+
+        public Scripture()
+        {
+
+        }
+    }
+}
+
